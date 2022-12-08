efb5847cca59b4e4b862f9da8222170fb937047d
lshiroma <scarylarriana@gmail.com>
2022-12-08 14:14:49 -0500

    progress on making nicer suggestion and bug reports
    not as functional as I was hoping yet, will iron out bugs later

--- cogs/bug_report.py
+++ cogs/bug_report.py
@@ -1,27 +1,38 @@
 #@Scarriana#5999
 #December 2022
 #Monumenta Discord Task Bot Prototype: bug report cog
-
-
+import config
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 
 class Bug_Report(commands.Cog):
 
-	def __init__(self, bot):
+	def __init__(self, bot: commands.Bot):
 		self.bot = bot
 
+
+	#sync bot: this is necessary, but also needs more than what I have now
+	@commands.command()
+	async def sync_report(self, ctx) -> None:
+		fmt = await ctx.bot.tree.sync(guild=ctx.guild)
+		await ctx.send(f'Synced{len(fmt)} commands.')
 	
+	#on ready
 	@commands.Cog.listener()
 	async def on_ready(self):
+
 		print('Bug Report cog loaded.')
 
-	@commands.command()
-	async def bugreport(self, ctx):
-		embed=discord.Embed(description='bug report')
-		await ctx.channel.send(embed=embed)
+	#bug report form
+	@app_commands.command(name="bugreport", description="bug report form")
 
-	
+	async def bugreport(self, interaction: discord.Interaction, tag: str, tag2: str):
+		print(str)
+		await interaction.response.send_message(f'Recieved: {str}')
+
+
+#setting up cog for the bot: will need to adjust for which discord servers get passed into guilds
 async def setup(bot):
-		await bot.add_cog(Bug_Report(bot))
+		await bot.add_cog(Bug_Report(bot), guilds=[discord.Object(id=config.GUILD_ID)])
--- cogs/suggestion.py
+++ cogs/suggestion.py
@@ -4,11 +4,23 @@
 
 import discord
 from discord.ext import commands
+from discord import ui, app_commands
+from datetime import datetime
+
+
+class suggestion_Modal(discord.ui.Modal, title="Suggestion Form"):
+	category = discord.ui.TextInput(label='Category', style=discord.TextStyle.short, placeholder='Label must be one of: misc plugin item class boss quest build mob cmd discord advancement delve server npc enchantment gui website', default='misc', required = True, max_length=11)
+	response = discord.ui.TextInput(label='Suggestion', style=discord.TextStyle.paragraph,required = True, max_length=1800)
+	async def on_submit(self, interaction: discord.Interaction):
+		#embeded answer
+		embed = discord.Embed(title = self.title, description = (f"**{self.category.label}**\n{self.category}\n" + f"**{self.response.label}**\n{self.response}"), timestamp= datetime.now(), color = discord.Color.blue())
+		embed.set_author(name=interaction.user, icon_url= interaction.user.avatar)
+		await interaction.response.send_message(embed=embed)
 
 
 class Suggestion(commands.Cog):
 
-	def __init__(self, bot):
+	def __init__(self, bot: commands.Bot):
 		self.bot = bot
 
 	
@@ -16,14 +28,28 @@ class Suggestion(commands.Cog):
 	async def on_ready(self):
 		print('Suggestion cog loaded.')
 
+	@commands.command(name="suggest", description="suggestion form")
+	async def suggest(self, interaction: discord.Interaction):
+		await interaction.response.send_modal(suggestion_Modal)
+
+		# user = interaction.user
+		# guild = interaction.guild
+		# channel = interaction.channel
+
 	@commands.command()
-	async def suggestion(self, ctx):
-		embed=discord.Embed(description='suggestion')
-		await ctx.channel.send(embed=embed)
+	#sync bot: this is necessary, but also needs more than what I have now
+	async def sync_sug(self, ctx) -> None:
+		fmt = await ctx.bot.tree.sync(guild=ctx.guild)
+		await ctx.send(f'Synced {len(fmt)} commands.')
+
+	# @commands.command()
+	# async def hello(self, ctx):
+	# 	await interaction.response.send_message("I am here I am seen I am synced")
 
 	
 async def setup(bot):
 		await bot.add_cog(Suggestion(bot))
+
 
 		
 
